df08088e2862e79eee87ce40c969d90bd6a93923
IschukM <dzubak@ukr.net>
2023-04-06 22:31:03 +0300

    make repo

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from 'pages/SharedLayout';
+import { lazy } from 'react';
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route to="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
